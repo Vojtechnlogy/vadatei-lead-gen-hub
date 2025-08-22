4d0fed4989cfbb33054f1aa27fc413772f756b9b
Vojtech Tolasz <vojtech.tolasznl@gmail.com>
2025-08-22 17:39:37 +0200

    change to Browser Routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,16 @@ import "./i18n"; // init i18n before rendering
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HelmetProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
-
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
