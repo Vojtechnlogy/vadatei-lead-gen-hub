56422f62c7e63597fd627d2f3ec8af41864188fb
Vojtech Tolasz <vojtech.tolasznl@gmail.com>
2025-08-27 16:15:23 +0200

    Small update to the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import metaMapPlugin from "vite-plugin-react-meta-map";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
@@ -12,10 +11,6 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    metaMapPlugin({
-      pageMetaMapFilePath: "./src/pageMetaMap.ts",
-      pageTemplateFilePath: "./src/PageTemplate.tsx",
-    }),
   ],
   resolve: {
     alias: {
